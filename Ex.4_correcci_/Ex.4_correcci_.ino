1cdb0e49f48ecf9024010dd2bf86e2c14bea7395
ebc96 <bonete.info4@gmail.com>
2015-05-11 22:11:49 +0200

    Ex.4 corregit
    No havia vist el programa que havia de fer i men havia inventat un.

---
+++ Ex.4_correcci_/Ex.4_correcci_.ino
@@ -0,0 +1,19 @@
+void setup()                // run once, when the sketch starts
+{
+ Serial.begin(9600);       // set up Serial library at 9600 bps
+ Serial.println("Escull el numero de l'operació que vol realitzar");  // prints hello
+ delay(1000);
+ Serial.println("  1. Comprobar numero de tarjeta de credit");  // prints hello
+ delay(1000);
+ Serial.println("  2. Comprovar numero de compte bancari");  // prints hello
+ delay(1000);
+ Serial.println("  3. Buscar un digit perdut de la tarjeta de credit ");  // prints hello
+ delay(1000);
+}
+
+void loop()                 // run over and over again
+{
+
+ 
+}
+
